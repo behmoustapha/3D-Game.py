75c8d6a06cbdc093631d6c8245dc9f95d18c8036
behmoustapha <diarrassouba2106@gmail.com>
2026-09-15 16:54:20 +0200

    fix mouvement, ajout de .json pour le spawn et la map, ajout de collision

---
+++ GameTest.py
@@ -0,0 +1,213 @@
+import pygame
+import math
+import time
+import json
+
+pygame.init()
+
+WIDTH, HEIGHT = 800, 400
+win = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("GTA VI 2")
+pygame.mouse.set_visible(True)
+pygame.event.set_grab(True)
+with open("map.json", "r") as file:
+    wmap = json.load(file)
+world_map = wmap
+with open("spawn.json", "r") as file:
+    sp = json.load(file)
+spawn = sp
+player_x, player_y = spawn
+player_angle = 0
+FOV = math.pi / 3
+num_rays = 160
+max_depth = 15
+clock = pygame.time.Clock()
+
+def respawning():
+    font = pygame.font.SysFont("Times", 15, bold=True)
+    bouton_war = pygame.Rect(WIDTH//2 - 100, HEIGHT//2 + 100, 200, 50)
+    win.fill((21,10,210))
+    time.sleep(2)
+    res = font.render("RESPAWNING", True, (255, 255, 255))
+    win.blit(res, (bouton_war.x + 100, bouton_war.y + 15))
+    time.sleep(2)
+
+def collisions(x, y):
+    nx = int(x)
+    ny = int(y)
+    if world_map[nx][ny] == 1:
+        return False
+    else:
+        return True
+
+def cast_rays():
+    global player_x, player_y
+    start_angle = player_angle - FOV / 2
+    for ray in range(num_rays):
+        ray_angle = start_angle + (ray / num_rays) * FOV
+        dist = 0
+        hit = False
+        
+        while not hit and dist < max_depth:
+            dist += 0.05
+            x = int(player_x + dist * math.cos(ray_angle))
+            y = int(player_y + dist * math.sin(ray_angle))
+            try:
+                if world_map[x][y] == 1:
+                    hit = True
+            except IndexError:
+                player_x, player_y = spawn
+                respawning()
+                break
+
+        h = 200 / (dist + 0.0001)
+        c = 125 / (1 + dist * dist * 0.1)
+        color = (c, c, c)
+        pygame.draw.rect(win, color, (ray * (WIDTH/num_rays), HEIGHT/2 - h/2, WIDTH/num_rays, h))
+
+
+
+def draw_crosshair():
+    cx, cy = WIDTH//2, HEIGHT//2
+    pygame.draw.line(win, (255,255,255), (cx-10, cy), (cx+10, cy), 2)
+    pygame.draw.line(win, (255,255,255), (cx, cy-10), (cx, cy+10), 2)
+    
+def show_menu(events):
+    global game_state
+    global press
+    global game_over
+    global keys
+    game_state = "menu"
+    pygame.mouse.set_visible(True)
+    win.fill((255,255,255))
+    pos = pygame.mouse.get_pos()
+    texte = pygame.font.SysFont("arial", 15, bold=True) 
+    
+    bouton_jeu = pygame.Rect(WIDTH//2 - 100, HEIGHT//2 - 60, 200, 50)
+    bouton_opt = pygame.Rect(WIDTH//2 - 100, HEIGHT//2 + 10, 200, 50)
+    bouton_war = pygame.Rect(WIDTH//2 - 100, HEIGHT//2 + 100, 200, 50)
+
+    pygame.draw.rect(win, (0, 0, 0), bouton_jeu)
+    pygame.draw.rect(win, (0, 0, 0), bouton_opt)
+    font = pygame.font.SysFont("Times", 15, bold=True)
+    text_jouer = font.render("Jouer", True, (255, 255, 255))
+    text_warning = font.render("Salut !", True, (0, 0, 0))
+    text_options = font.render("Options", True, (255, 255, 255))
+    win.blit(text_jouer, (bouton_jeu.x + 70, bouton_jeu.y + 15))
+    win.blit(text_options, (bouton_opt.x + 60, bouton_opt.y + 15))
+    win.blit(text_warning, (bouton_war.x + 100, bouton_war.y + 15))
+
+
+    pygame.display.update()
+
+    for e in pygame.event.get():
+        if e.type == pygame.MOUSEBUTTONDOWN and e.button == 1:
+            if bouton_jeu.collidepoint(pos):
+                game_state = "jeu"
+            elif bouton_opt.collidepoint(pos):
+                game_state = "options"
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_ESCAPE]:
+            game_over = True 
+
+
+def show_options(events):
+    global game_state
+    global pose
+    global keys
+    game_state = "options"
+    win.fill((255,255,255))
+    pose = pygame.mouse.get_pos()
+    for e in pygame.event.get():
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_ESCAPE]:
+            game_state = "menu"
+    pygame.display.update()
+
+def play_game(events):
+    global game_state
+    global game_over
+    global keys
+    global player_angle
+    global player_x
+    global player_y
+    game_state = "jeu"
+    pygame.mouse.set_visible(False)
+
+    for e in events:
+        if e.type == pygame.QUIT:
+            game_over = True
+            return
+    
+        keys = pygame.key.get_pressed()
+        
+        # Movement
+        cos_a = math.cos(player_angle)
+        sin_a = math.sin(player_angle)
+
+    if keys[pygame.K_z]:    
+            newx = player_x + math.cos(player_angle) * 0.05
+            newy = player_y + math.sin(player_angle) * 0.05
+            check = collisions(newx, newy)
+            if check == True:
+                player_x += math.cos(player_angle) * 0.05
+                player_y += math.sin(player_angle) * 0.05
+            else:
+                pass
+    if keys[pygame.K_s]:
+            newx = player_x - math.cos(player_angle) * 0.05
+            newy = player_y - math.sin(player_angle) * 0.05
+            check = collisions(newx, newy)
+            if check == True:
+                player_x -= math.cos(player_angle) * 0.05
+                player_y -= math.sin(player_angle) * 0.05
+            else:
+                pass
+    if keys[pygame.K_q]:
+            newx = player_x + math.sin(player_angle) * 0.025
+            newy = player_y - math.cos(player_angle) * 0.025
+            check = collisions(newx, newy)
+            if check == True:
+                player_x += math.sin(player_angle) * 0.025
+                player_y -= math.cos(player_angle) * 0.025
+            else:
+                pass
+    if keys[pygame.K_d]:  
+            newx = player_x - math.sin(player_angle) * 0.025
+            newy = player_y + math.cos(player_angle) * 0.025
+            check = collisions(newx, newy)
+            if check == True:
+                player_x -= math.sin(player_angle) * 0.025
+                player_y += math.cos(player_angle) * 0.025
+            else:
+                pass
+    if keys[pygame.K_ESCAPE]:
+        game_state = "menu"
+        pygame.event.set_grab(False)
+        pygame.mouse.set_visible(True)
+
+    # Mouse movement
+    dx, dy = pygame.mouse.get_rel()
+    player_angle += dx * 0.002
+
+    win.fill((0,0,0))
+    cast_rays()
+    draw_crosshair()
+    pygame.display.flip()
+    clock.tick(60)
+
+game_state = "menu"
+game_over = False
+while not game_over:
+    events = pygame.event.get()
+    for e in events:
+        if e.type == pygame.QUIT:
+            game_over = True
+    if game_state == "menu":
+        show_menu(events)
+    elif game_state == "options":
+        show_options(events)
+    elif game_state == "jeu":
+        play_game(events)
+pygame.quit()
+quit()
